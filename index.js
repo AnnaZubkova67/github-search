168bf219f728bfefcc080a973a641bc15c0624e2
Anna Zubkova <annazubkowa12@gmail.com>
2022-08-17 19:06:17 +0400

    commit

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-const app = document.querySelector('.app');
-
 function elementCreation(tagName, className, place) {
     let newElement = document.createElement(tagName);
     newElement.classList.add(className)
@@ -7,25 +5,29 @@ function elementCreation(tagName, className, place) {
     return newElement;
 }
 
-const title = elementCreation('hi', 'app__title', app)
-title.textContent = 'Github search repositories'
+const app = document.querySelector('.app');
+const title = elementCreation('hi', 'app__title', app);
+title.textContent = 'Github search repositories';
 const entryField = elementCreation('input', 'app__input', app);
-const olRepo = elementCreation('ol', 'app__olRepo', app)
+const olRepo = elementCreation('ol', 'app__olRepo', app);
 const olFavorites = elementCreation('ol', 'app__olFavorites', app);
 
 const debounce = (fn, debounceTime) => {
     let timer;
     return function () {
-        clearTimeout(timer)
+        clearTimeout(timer);
         timer = setTimeout(() => {
-            fn.apply(this, arguments)
+            fn.apply(this, arguments);
         }, debounceTime)
     }
 };
 
 function serverCall() {
     olRepo.innerHTML = '';
-    const data = entryField.value
+    const data = entryField.value;
+    if (!data) {
+        return;
+    }
 
     fetch(`https://api.github.com/search/repositories?q=${data}`)
         .then(response => response.json())
@@ -36,10 +38,10 @@ function serverCall() {
 
                 if (elem.name.includes(data) && countElement < 5 && data) {
                     const list = elementCreation('li', 'app__listRepositories', olRepo);
-                    list.textContent = elem.name
+                    list.textContent = elem.name;
                     countElement++;
 
-                    list.addEventListener('click', async () => {
+                    list.addEventListener('click', () => {
                         entryField.value = '';
                         olRepo.innerHTML = '';
                         const favoritesList = elementCreation('li', 'app__favoritesList', olFavorites);
@@ -50,19 +52,19 @@ function serverCall() {
                         buttonClose.textContent = 'X';
 
                         buttonClose.addEventListener('click', () => {
-                            let elt = buttonClose.closest('li')
-                            elt.remove()
+                            let elt = buttonClose.closest('li');
+                            elt.remove();
                         })
                     })
                 }
             })
 
 
-        })
+        }).catch(e => console.log(e.message = 'Не удалось выполнить запрос'));
 }
 
-const request = debounce(serverCall, 700)
-entryField.addEventListener('keydown', request)
+const request = debounce(serverCall, 500);
+entryField.addEventListener('keydown', request);
 
 
 
